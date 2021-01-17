563e3cc6a86343b8eb9dc591ba5cd604edeb584e
rexendevar <46948713+rexendevar@users.noreply.github.com>
2021-01-18 00:25:25 +0800

    Fixed install issues
    Replaced all references to scipy's toimage function with a copy of it in a separate file. This prevents the user from having to deal with installing deprecated scipy stuff. Also added the complete list of imagenet classes to a separate TXT file so you don't need to look them up and try to figure out which list is correct.

--- visualize.py
+++ visualize.py
@@ -4,7 +4,7 @@ import numpy as np
 import moviepy.editor as mpy
 import random
 import torch
-from scipy.misc import toimage
+from fakescipy import toimage
 from tqdm import tqdm
 from pytorch_pretrained_biggan import (BigGAN, one_hot_from_names, truncated_noise_sample,
                                        save_as_images, display_in_terminal)
